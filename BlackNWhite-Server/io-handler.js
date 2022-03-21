37e54177a208345c3ca88b3a35a16f66c68b1585
HyeM207 <63229014+HyeM207@users.noreply.github.com>
2022-03-21 16:49:53 +0900

    Update io-handler.js

--- BlackNWhite-Server/io-handler.js
+++ BlackNWhite-Server/io-handler.js
@@ -7,31 +7,31 @@ module.exports = (io) => {
     
     var gameserver = io.of("blacknwhite");
  
+    var rooms ={}; 
     let Players = [];
 
-    gameserver.on('connection', (socket) => {
+    io.on('connection', (socket) => {
         console.log("io-handler.js socket connect!!");
         console.log("socketid : "+ socket.id); 
         
-        // const {ns} = url.parse(socket.handshake.url, true).query;
-        // console.log(ns);
-    
-        socket.emit('PlayerConnected');
     
     
+        // socket.on('PlayerEnter', function() {
+        //     console.log('A Player ~~!~!~');
+        // });
+
+
         socket.on('PlayerEnter', function(nickname) {
             console.log("Players >> ");
             let playerInfo = [socket.id, nickname, "false", "false", "0", "7"];
             Players[Players.length]=playerInfo;
             console.log("Players >> ", Players);
-
             socket.emit('PlayersData', Players);
         });
 
-     
     
         socket.on('disconnect', function() {
-            console.log('A Player disconnected');
+            console.log('A Player disconnected!!!');
         });
     })
 }

7d6a222de8b7dde85684d06e6eb60a0cb63a86d9
woogy4231 <woogy4231@naver.com>
2022-03-26 18:44:51 +0900

    connect DB

--- BlackNWhite-Server/server_functions/db_func.js
+++ BlackNWhite-Server/server_functions/db_func.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const Test = require('../schemas/test');
+// const Room = require("../schemas/room");
+const ObjectId = require('mongodb').ObjectId;
 
-/*
+
 //===== Mongo DB ====
 //MongoDB 연결
-mongoose.connect('mongodb://localhost:27017/nodejs'); // 포트번호 뒤에 nodejs는 사용할 DB 이름 (현재는 nodejs DB를 사용)
+mongoose.connect('mongodb://localhost:27017/BlackNWhite'); // 포트번호 뒤에 nodejs는 사용할 DB 이름 (현재는 nodejs DB를 사용)
 var db = mongoose.connection;
 
 // 연결 실패
@@ -14,4 +17,3 @@ db.on('error', function(){
 db.once('open', function() {
     console.log('Connected!');
 });
-*/

452517654973c8fadfed703a975f23ab8dc99483
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-03-26 13:25:09 +0900

    Add status change func

--- BlackNWhite-Server/io-handler.js
+++ BlackNWhite-Server/io-handler.js
@@ -9,6 +9,9 @@ module.exports = (io) => {
  
     var rooms ={}; 
     let Players = [];
+    let gamePlayer = {};
+    let evenNumPlayer = false;
+    let numPlayer = 1;
 
     io.on('connection', (socket) => {
         console.log("io-handler.js socket connect!!");
@@ -23,22 +26,39 @@ module.exports = (io) => {
 
         socket.on('PlayerEnter', function(nickname) {
             console.log("Players >> ");
-            let playerInfo = [socket.id, nickname, "false", "false", "0", "7"];
-            Players[Players.length]=playerInfo;
+            const rand_Color = Math.floor(Math.random() * 12);
+            // eval("Players.player" + numPlayer + " = playerInfo")
+            let playerOrder = "player" + numPlayer;
+            let playerInfo = {playerOrder: playerOrder, socket: socket.id, nickname: nickname, readyStatus: false, teamStatus: false, team: evenNumPlayer, color: rand_Color};
+            Players.push(playerInfo);
+            gamePlayer.player = Players;
+            // Players[Players.length]=playerInfo;
+            console.log("PlayersInfo", numPlayer, " >> ", playerInfo);
             console.log("Players >> ", Players);
+            console.log("gmaePlayer >> ", gamePlayer);
+
+            if (evenNumPlayer == false){
+                evenNumPlayer = true;
+            } else {
+                evenNumPlayer = false;
+            }
+
+            numPlayer = numPlayer + 1;
             
-
             // JSON 형식으로 유니티에 데이터 보내기
-            Players = {socket : "abcd", nickname: "efg", readyStatus:"false", teamStatus: "false", team: "0", color: "7"};
 
-            console.log("json : ", Players);
-
-            const PlayersJson = JSON.stringify(Players);
+            var PlayersJson = JSON.stringify(gamePlayer);
             console.log("jsonStringify : ", PlayersJson.toString());
             socket.emit('PlayersData', PlayersJson);
         });
+        
+        socket.on('changeStatus', function(jsonStr) {
+            gamePlayer = JSON.parse(jsonStr);
 
-    
+            console.log('new Player info Jsong string : ', jsonStr);
+            socket.emit('PlayersData', jsonStr);
+        });
+        
         socket.on('disconnect', function() {
             console.log('A Player disconnected!!!');
         });

55753845457ea1a2e7d92347976c67adcc491d6b
woogy4231 <woogy4231@naver.com>
2022-05-20 20:46:24 +0900

    update io-handler

--- BlackNWhite-Server/schemas/roomTotal/Section.js
+++ BlackNWhite-Server/schemas/roomTotal/Section.js
@@ -5,6 +5,7 @@ const progress = require('./Progress').schema;
 const Section = new Schema({
     destroyStatus  : { type : Boolean, required : true },
     level  : { type : Number, required : true },
+    vuln : { type : Number, required : true },
     attack : { type : progress, required : true },
     response : { type : progress, required : true },
 })
